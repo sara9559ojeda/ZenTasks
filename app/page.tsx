eead9249831d539ad5753c3d29db3d1a1c8635ed
sara9559ojeda <sara.ojeda@campusucc.edu.co>
2024-09-17 22:31:15 -0500

    mensaje

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,6 @@ export default function Home() {
             </svg>
             <p className="pb-2">Your boards</p>
           </section>
-
           <div className="flex rounded-xl flex-row flex-wrap p-5 overflow-y-auto bg-neutral-900  justify-center items-center ">
             <div className=" grid grid-rows-6 rounded-xl bg-neutral-600 overflow-hidden m-7 w-[240px] h-[235px] p-4">
               <p className="font-bold">Board title</p>
@@ -118,14 +117,11 @@ export default function Home() {
               </svg>
             </div>
           </div>
-
-          
             <section id="members " className=" hidden sm:block " >
               <section className="flex"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="flex size-6 p-1 stroke-neutral-300">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
               </svg>
               Your friends</section>
-              
               <div className="flex ">
                 <div className="grid grid-rows-1 pt-2">
                   <img className=" m-3 w-20 h-20  size-11 rounded-full" src="https://img.freepik.com/foto-gratis/retrato-hombre-reir_23-2148859448.jpg " alt="" />
@@ -156,15 +152,9 @@ export default function Home() {
                   <p className="text-center">Michael</p>
                 </div>
               </div>
-
             </section>
-          
-
-
         </menu>
-
       </div>
-
     </main>
   );
 }
